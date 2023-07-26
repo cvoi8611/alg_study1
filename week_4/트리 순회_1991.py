a5e77c72b7e63474b96188dcbb40acb6601f2a42
covi_d <covi@covisserver.mshome.net>
2023-07-26 17:23:08 +0000

    4주차 과제 업로드

---
+++ week_4/트리 순회_1991.py
@@ -0,0 +1,65 @@
+'''
+n = int(input())
+inputs = []
+print(inputs)
+print(n)
+
+
+for _ in range(n):
+    inputs.append(input().split())
+    
+class Node():
+    def __init__(self,item):
+        self.data = item
+        self.left = None
+        self.right = None
+        
+def preorder(node):
+    print(node.item)
+'''
+    
+    
+    
+    
+n = int(input())
+inputs = []
+for _ in range(n):
+    inputs.append(input().split())
+
+class Node():
+    def __init__(self, item, left, right):
+        self.item = item
+        self.left = left
+        self.right = right
+
+def preorder(node):
+    print(node.item, end = '')
+    if node.left != '.':
+        preorder(tree[node.left])
+    if node.right != '.':
+        preorder(tree[node.right])
+        
+def inorder(node):
+    if node.left != '.':
+        inorder(tree[node.left])
+    print(node.item, end = '')
+    if node.right != '.':
+        inorder(tree[node.right])
+        
+def postorder(node):
+    if node.left != '.':
+        postorder(tree[node.left])
+    if node.right != '.':
+        postorder(tree[node.right])
+    print(node.item, end = '')
+
+tree = {}
+for item, left, right in inputs:
+    tree[item] = Node(item, left, right)
+    
+preorder(tree['A'])
+print()
+inorder(tree['A'])
+print()
+postorder(tree['A'])
+print()
